2ce3f33d28c2f5c070c38008153f68e36c84415d
Prashant Marathay <pmarathay@infiniteoptions.com>
2024-08-21 20:59:34 -0700

    before zappa

--- movies_api.py
+++ movies_api.py
@@ -7,25 +7,30 @@
 print("In Movies 2")
 
 import os
-# import boto3
+import boto3
 # import json
 # import sys
 # import json
 import pytz
 import pymysql
-# import requests
+import requests
 # import stripe
 # from fuzzywuzzy import fuzz
+import numpy as np
+import pandas as pd
+
 
 from dotenv import load_dotenv
 from datetime import date, datetime
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
 # from flask_mail import Mail, Message
 from werkzeug.exceptions import BadRequest, InternalServerError
 from decimal import Decimal
 # from hashlib import sha512
+from gensim.models import Word2Vec
+from io import StringIO
 
 #  NEED TO SOLVE THIS
 # from NotificationHub import Notification
@@ -94,7 +99,6 @@ def getNow():
 
 
 
-
 # --------------- DATABASE CONFIGUATION ------------------
 # Connect to MySQL database (API v2)
 def connect():
@@ -105,11 +109,11 @@ def connect():
     global RDS_DB
 
     print("Trying to connect to RDS (API v2)...")
-    print("RDS_HOST: ", os.getenv('RDS_HOST'))
-    print("RDS_USER: ", os.getenv('RDS_USER'))
-    print("RDS_PORT: ", os.getenv('RDS_PORT'), type(os.getenv('RDS_PORT')))
-    print("RDS_PW: ", os.getenv('RDS_PW'))
-    print("RDS_DB: ", os.getenv('RDS_DB'))
+    # print("RDS_HOST: ", os.getenv('RDS_HOST'))
+    # print("RDS_USER: ", os.getenv('RDS_USER'))
+    # print("RDS_PORT: ", os.getenv('RDS_PORT'), type(os.getenv('RDS_PORT')))
+    # print("RDS_PW: ", os.getenv('RDS_PW'))
+    # print("RDS_DB: ", os.getenv('RDS_DB'))
 
    
     try:
@@ -227,7 +231,120 @@ def runSelectQuery(query, cur):
 
 # -- Queries start here -------------------------------------------------------------------------------
 
+def get_model_from_s3():
+    s3_access_key = os.getenv('AWS_ACCESS_KEY_ID')
+    s3_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    s3_bucket_name = os.getenv('BUCKET_NAME')
+    s3_file_key_word2vec_model = os.getenv('S3_PATH_KEY_WORD2VEC_MODEL')
 
+    # print("s3_access_key: ", os.getenv('AWS_ACCESS_KEY_ID'))
+    # print("s3_secret_key: ", os.getenv('AWS_SECRET_ACCESS_KEY'))
+    # print("s3_bucket_name: ", os.getenv('BUCKET_NAME'))
+    # print("s3_file_key_word2vec_model: ", os.getenv('S3_PATH_KEY_WORD2VEC_MODEL'))
+
+    s3_client = boto3.client('s3', aws_access_key_id=s3_access_key, aws_secret_access_key=s3_secret_key)
+    # after s3 connect
+
+    local_model_path = '/tmp/word2vec_movie_ratings_embeddings.model'
+    s3_client.download_file(s3_bucket_name, s3_file_key_word2vec_model, local_model_path)
+    # model downloaded from s3
+
+    model = Word2Vec.load(local_model_path)
+    return model
+
+
+def get_genres_from_s3():
+    s3_access_key = os.getenv('AWS_ACCESS_KEY_ID')
+    s3_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    s3_bucket_name = os.getenv('BUCKET_NAME')
+    s3_file_key_genres = os.getenv('S3_PATH_KEY_GENRES')
+
+    # print("s3_access_key: ", os.getenv('AWS_ACCESS_KEY_ID'))
+    # print("s3_secret_key: ", os.getenv('AWS_SECRET_ACCESS_KEY'))
+    # print("s3_bucket_name: ", os.getenv('BUCKET_NAME'))
+    # print("s3_file_key_word2vec_model: ", os.getenv('S3_PATH_KEY_GENRES'))
+
+    s3_client = boto3.client('s3', aws_access_key_id=s3_access_key, aws_secret_access_key=s3_secret_key)
+
+    genres_response = s3_client.get_object(Bucket=s3_bucket_name, Key=s3_file_key_genres)
+
+    genres_csv_content = genres_response['Body'].read().decode('utf-8')
+    genres_cleaned = pd.read_csv(StringIO(genres_csv_content))
+    return genres_cleaned
+
+
+def generate_user_profile(ratings, model, genres_cleaned):
+    user_vector = np.zeros(model.vector_size)
+    total_weight = 0.0
+
+    for title, rating in ratings.items():
+        movie_id = genres_cleaned[genres_cleaned['title'] == title]['movieId'].values[0]
+        if int(movie_id) in model.wv:
+            user_vector += model.wv[int(movie_id)] * rating
+            total_weight += rating
+
+    if total_weight > 0:
+        user_vector /= total_weight
+
+    return user_vector
+
+def recommend_movies(user_vector, model, metadata, top_n=10):
+    similarity_scores = []
+
+    for movie_id in model.wv.index_to_key:
+        movie_vector = model.wv[movie_id]
+        user_norm = np.linalg.norm(user_vector)
+        movie_norm = np.linalg.norm(movie_vector)
+
+        if user_norm == 0 or movie_norm == 0:
+            similarity = 0
+        else:
+            similarity = np.dot(user_vector, movie_vector) / (user_norm * movie_norm)
+
+        similarity_scores.append((movie_id, similarity))
+
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    top_recommendations = similarity_scores[:top_n]
+
+    recommended_movies = []
+    for movie_id, score in top_recommendations:
+        movie_data = metadata[metadata['movieId'] == int(movie_id)].iloc[0]
+        recommended_movies.append({
+            'movieId': movie_id,
+            'title': movie_data['title'],
+            'genres': movie_data['genres']
+        })
+
+    return recommended_movies
+
+
+def fetch_tmdb_data(movie_title):
+    # print("In Fetch TMDB: ", movie_title)
+    api_key = os.getenv('TMDB_API_KEY')
+    # print("TMDB Key: ", api_key)
+    search_url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_title}"
+    response = requests.get(search_url)
+    search_data = response.json()
+
+    if search_data['results']:
+        movie_id = search_data['results'][0]['id']
+        movie_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
+        credits_url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={api_key}&language=en-US"
+
+        movie_response = requests.get(movie_url)
+        credits_response = requests.get(credits_url)
+
+        movie_data = movie_response.json()
+        credits_data = credits_response.json()
+
+        return {
+            "tmdb_id": movie_id,
+            "poster": f"https://image.tmdb.org/t/p/w500{movie_data.get('poster_path')}",
+            "overview": movie_data.get('overview', 'Overview not available'),
+            "rating": f"{movie_data.get('vote_average', 'Not rated')}/10 ({movie_data.get('vote_count', 0)} votes)",
+            "cast": ", ".join([actor['name'] for actor in credits_data.get('cast', [])[:5]])
+        }
+    return None
 
 class test_api(Resource):
     def get(self):
@@ -264,6 +381,38 @@ class test_db(Resource):
         finally:
             disconnect(conn)
 
+class profile_recs(Resource):
+    def post(self):
+        print("In Profile Recommendations")
+        user_input = request.json
+        # print("User data received: ", user_input)
+        ratings = user_input.get('ratings', {})
+        # print("Data stored: ", ratings)
+
+        if not ratings:
+            return {"error": "No ratings provided"}, 400
+
+        
+        word2vec_model = get_model_from_s3()
+        genres_cleaned = get_genres_from_s3()
+
+        # Generate user profile using titles
+        user_vector = generate_user_profile(ratings, word2vec_model, genres_cleaned)
+        recommendations = recommend_movies(user_vector, word2vec_model, genres_cleaned, top_n=10)
+
+        # Add TMDb data to recommendations
+        detailed_recommendations = []
+        for recommendation in recommendations:
+            # print("Inside for loop", recommendation)
+            movie_title = recommendation['title']
+            tmdb_info = fetch_tmdb_data(movie_title)
+
+            if tmdb_info:
+                recommendation.update(tmdb_info)
+            detailed_recommendations.append(recommendation)
+
+        return jsonify({"recommended_movies": detailed_recommendations})
+
 
 # -- DEFINE APIS -------------------------------------------------------------------------------
 
@@ -273,6 +422,7 @@ class test_db(Resource):
 
 api.add_resource(test_api, '/api/v2/testAPI')
 api.add_resource(test_db, '/api/v2/testDB')
+api.add_resource(profile_recs, '/api/v2/profile')
 
 # api.add_resource(NewEndpoint, "/api/v2/newEndpoint")
 # Run on below IP address and port
